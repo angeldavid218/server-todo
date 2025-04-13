1f6f2dad71cf3d468aa22b1913f92edd04b5d0d0
Angel Serrano <angelserrano@Angels-MacBook-Pro.local>
2025-04-13 16:25:20 -0700

    feat: todo component markup and css changes

---
+++ src/components/Todo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Todo() {
+  return (
+    <div className="text-3xl bg-white w-1/2 rounded-sm mt-4 h-13">
+      my todo list
+    </div>
+  );
+}
